06286c86b128f39ee3f5555928665bab17248ef2
Banila <royng34@gmail.com>
2021-07-23 18:32:37 +0800

    Nothing much

--- src/components/WIP/Loader.jsx
+++ src/components/WIP/Loader.jsx
@@ -1,4 +1,6 @@
-import logo from '../assets/animat-road-trip-color.gif';
+// drag this to the Loader folder
+import logo from './animat-road-trip-color.gif';
+
 
 const Loader = props => (
     <div className="loader--container" {...props}>
